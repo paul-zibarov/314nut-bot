a68e76b7e86a0169af54290b666b5302847b2317
Serzh108 <serzh@gabris.kiev.ua>
2021-03-27 18:22:19 +0200

    complited p.1 - 10

--- src/Pages/BurnPage/BurnPage.js
+++ src/Pages/BurnPage/BurnPage.js
@@ -1,34 +1,86 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import Web3 from 'web3';
+import abiFront from '../../constants/abis/abiFront.json';
 import { addBalance } from '../../redux/hkOperations';
 import styles from './BurnPage.module.css';
 
 const INITIAL_STATE = {
   pi: '',
-  get: '',
+  get: 'PETH',
+};
+
+const INITIAL_STATE_RESIVED = {
+  ETH: 0,
+  PBTC: 0,
+  PUSD: 0,
 };
 
 export default function BurnPage() {
   const [state, setState] = useState(INITIAL_STATE);
+  const [receivedAmount, setReceivedAmount] = useState(INITIAL_STATE_RESIVED);
   const balance = useSelector(state => state.hk.balance);
   const dispatch = useDispatch();
 
-  // const inputHandler = e => {
-  //   const value = e.target.value;
-  //   setState(value);
-  // };
+  const web3 = new Web3(
+    Web3.givenProvider ||
+      'https://ropsten.infura.io/v3/5f1cc39aff43406b9cbbab0cc9383c98',
+  );
+  const FRONT_CONTRACT_KEY = '0xDB0f0c98828980413239312207eAaDaD663B2326';
+  const FRONT_CONTRACT = new web3.eth.Contract(abiFront, FRONT_CONTRACT_KEY);
 
   const inputHandler = ({ target }) => {
     const { name, value } = target;
-    setState({ [name]: value });
+    setState(prev => ({ ...prev, [name]: value }));
   };
 
-  const burnHandler = () => {
-    console.log('burnHandler !', state);
-    dispatch(addBalance(state.pi));
-  };
+  // const burnHandler = () => {
+  //   console.log('burnHandler !', state);
+  //   dispatch(addBalance(state.pi));
+  // };
+  // ------- = Test Contract = -------
+  // --=-- helpful functions
+  const convertValue = value =>
+    Math.round(+value * 10000).toString() + '00000000000000';
+  const reduceValue = value => +value / 1e18;
 
+  // --- TEMP! - address ---
+  const address = '0x9b3dCD8AA0fCC5d6dEa920a2DA28309908Fa8A70';
+
+  const burnHandler = async () => {
+    if (state.get === 'PETH') {
+      const result = await FRONT_CONTRACT.methods.burn(state.pi).call();
+      console.log('burn = ', result);
+      const result2 = await FRONT_CONTRACT.methods
+        .getBurnAmount(state.pi)
+        .call();
+      console.log(
+        'getBurnAmount -> token0Amount = ',
+        reduceValue(result2.token0Amount),
+      );
+      console.log(
+        'getBurnAmount -> token1Amount = ',
+        reduceValue(result2.token1Amount),
+      );
+      setReceivedAmount(prev => ({
+        ...prev,
+        ETH: reduceValue(result2.token0Amount),
+        PBTC: reduceValue(result2.token1Amount),
+      }));
+    } else {
+      const result3 = await FRONT_CONTRACT.methods
+        .burnAndConvert(state.pi, address)
+        .call();
+      console.log('burnAndConvert = ', result3);
+      const result4 = await FRONT_CONTRACT.methods
+        .getBurnAmountConvert(state.pi, address)
+        .call();
+      console.log('BurnAmountConvert = ', result4);
+      setReceivedAmount(prev => ({ ...prev, PUSD: reduceValue(result4) }));
+    }
+  };
+  // ------- = /Test Contract = -------
   return (
     <>
       <p className={styles.mainBalance}>
@@ -81,7 +133,12 @@ export default function BurnPage() {
             get PUSD
           </label>
         </div>
-        <p className={styles.receive}>You will receive ___ PUSD and __ PBTC</p>
+        <p className={styles.receive}>
+          You will receive{' '}
+          {state.get === 'PETH'
+            ? `${receivedAmount.ETH} ETH and ${receivedAmount.PBTC} PBTC`
+            : `${receivedAmount.PUSD} PUSD`}
+        </p>
       </div>
       <p className={styles.receive}>1 PI = 100 USDT</p>
     </>
--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -1,25 +1,116 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import Web3 from 'web3';
+import abiFront from '../../constants/abis/abiFront.json';
 import styles from './MainPage.module.css';
 
 const INITIAL_STATE = {
   pbtc: '',
   pusd: '',
   coin: '',
+  sent: 'ETH',
 };
 
 export default function MainPage() {
+  const web3 = new Web3(
+    Web3.givenProvider ||
+      'https://ropsten.infura.io/v3/5f1cc39aff43406b9cbbab0cc9383c98',
+  );
+  const FRONT_CONTRACT_KEY = '0xDB0f0c98828980413239312207eAaDaD663B2326';
+  const FRONT_CONTRACT = new web3.eth.Contract(abiFront, FRONT_CONTRACT_KEY);
+
   const [state, setState] = useState(INITIAL_STATE);
+  const [index, setIndex] = useState(0);
+  const [burnAmountConvert, setBurnAmountConvert] = useState(0);
+
+  useEffect(() => {
+    const BurnAmountConvert = async () => {
+      const amountTokens = '1000000000000000000';
+      const convertToken = '0xc11090b333e0a8a88cb5d26f1f663cf859fcb861';
+      const result = await FRONT_CONTRACT.methods
+        .getBurnAmountConvert(amountTokens, convertToken)
+        .call();
+      console.log('BurnAmountConvert = ',reduceValue(result));
+      setBurnAmountConvert(reduceValue(result));
+    };
+    BurnAmountConvert();
+  }, []);
 
   const inputHandler = ({ target }) => {
     const { name, value } = target;
-    setState({ [name]: value });
+    setState(prev => ({ ...prev, [name]: value }));
   };
 
   const submitHandler = e => {
     e.preventDefault();
     console.log('submitHandler !');
   };
+  // ------- = Test Contract = -------
+  // --=-- helpful functions
+  const convertValue = value =>
+    Math.round(+value * 10000).toString() + '00000000000000';
+  const reduceValue = value => +value / 1e18;
+
+  const handlePBTC = async () => {
+    if (state.sent !== 'ETH') {
+      return;
+    }
+    const result = await FRONT_CONTRACT.methods
+      .getAmountOut(state.pbtc, true)
+      .call();
+    console.log(
+      'counterTokenAmount = ',
+      result.counterTokenAmount,
+      'indexAmount = ',
+      result.indexAmount,
+    );
+    setIndex(reduceValue(result.indexAmount));
+  };
+
+  // --- TEMP! - address ---
+  const address1 = '0x9b3dCD8AA0fCC5d6dEa920a2DA28309908Fa8A70';
+
+  const handleETH = async () => {
+    if (state.sent === 'ETH') {
+      const result = await FRONT_CONTRACT.methods
+        .getAmountOut(state.pusd, false)
+        .call();
+      console.log(
+        'counterTokenAmount = ',
+        result.counterTokenAmount,
+        'indexAmount = ',
+        result.indexAmount,
+      );
+      setIndex(reduceValue(result.indexAmount));
+    } else {
+      const result1 = await FRONT_CONTRACT.methods
+        .getAmountOutConvert(state.pusd, address1)
+        .call();
+      console.log('AmountOutConvert = ', result1);
+      setIndex(reduceValue(result1));
+    }
+  };
+
+  // --- TEMP! - address ---
+  const address = '0x9b3dCD8AA0fCC5d6dEa920a2DA28309908Fa8A70';
+
+  const mintButtonClick = async () => {
+    if (state.sent === 'ETH') {
+      const result = await FRONT_CONTRACT.methods
+        .mint(state.pbtc, state.pusd)
+        .call();
+      console.log('indexAmount 2x = ', result);
+      // setIndex(reduceValue(result));
+    } else {
+      const result = await FRONT_CONTRACT.methods
+        .mintAndConvert(state.pusd, address)
+        .call();
+      console.log('indexAmount 1x = ', result);
+      // setIndex(reduceValue(result));
+    }
+  };
+
+  // ------- = /Test Contract = -------
 
   return (
     <>
@@ -27,43 +118,83 @@ export default function MainPage() {
         your balance:<span>12 PI</span>
       </p>
       <p className={styles.tab}>
-        <NavLink to="/" className={styles.link} activeClassName={styles.activeLink}>mint</NavLink>
-        <NavLink to="/burn" className={styles.link}>burn</NavLink>
+        <NavLink
+          to="/"
+          className={styles.link}
+          activeClassName={styles.activeLink}
+        >
+          mint
+        </NavLink>
+        <NavLink to="/burn" className={styles.link}>
+          burn
+        </NavLink>
       </p>
-      <form className={styles.formWrapper} onSubmit={submitHandler}>
-        <div className={styles.inputTitle}>
+      <form className={styles.formWrapper}>
+        {/* {state.sent === 'ETH' ? 'visible' : 'hidden'} */}
+        <div
+          className={styles.inputTitle}
+          style={{ visibility: state.sent === 'ETH' ? 'visible' : 'hidden' }}
+        >
           <p>PBTC</p>
           <p className={styles.smallText}>Your balance: 132 PBTC</p>
         </div>
         <input
+          style={{ visibility: state.sent === 'ETH' ? 'visible' : 'hidden' }}
           type="text"
           name="pbtc"
           value={state.pbtc}
           onChange={inputHandler}
+          onBlur={handlePBTC}
         />
-        <p className={styles.inputTitle}>
+        {/* <p className={styles.inputTitle}>
           <select name="coin" value={state.coin} onChange={inputHandler}>
             <option value="PUSD">PUSD</option>
             <option value="PUAH">PUAH</option>
             <option value="PEUR">PEUR</option>
           </select>
           <span className={styles.smallText}>Your balance: 13 PUSD</span>
-        </p>
-        {/* <p className={styles.inputTitle}>
-          PUSD<span className={styles.smallText}>Your balance: 13 PUSD</span>
         </p> */}
+        <p className={styles.inputTitle}>
+          {state.sent === 'ETH' ? 'ETH' : 'PUSD'}
+          <span className={styles.smallText}>Your balance: 13 PUSD</span>
+        </p>
         <div style={{ display: 'flex' }}>
           <input
             type="text"
             name="pusd"
             value={state.pusd}
             onChange={inputHandler}
+            onBlur={handleETH}
           />
-          <button type="submit">mint</button>
+          <button type="button" onClick={mintButtonClick}>
+            mint
+          </button>
         </div>
-        <p className={styles.receive}>You will receive ___ PI</p>
+        <div className={styles.radioWrapper}>
+          <label>
+            <input
+              type="radio"
+              name="sent"
+              checked={state.sent === 'ETH'}
+              value="ETH"
+              onChange={inputHandler}
+            />
+            sent ETH + PBTC
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="sent"
+              checked={state.sent === 'PUSD'}
+              value="PUSD"
+              onChange={inputHandler}
+            />
+            sent PUSD
+          </label>
+        </div>
+        <p className={styles.receive}>{`You will receive ${index} PI`}</p>
       </form>
-      <p className={styles.receive}>1 PI = 100 USDT</p>
+      <p className={styles.receive}>{`1 PI = ${burnAmountConvert} USDT`}</p>
     </>
   );
 }
